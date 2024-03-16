6a42c9dafece672fdaf363b4ef6e83576be069dc
kasdmusic <lecoindre.kenzo@gmail.com>
2024-03-16 18:24:20 +0100

    Swiper

--- Tests/app/build.gradle.kts
+++ Tests/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.6.0")
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("com.github.yuyakaido:CardStackView:v2.3.4")
+    implementation ("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Tests/app/src/main/java/com/example/tests/ui/swiper/SwiperFragment.java
+++ Tests/app/src/main/java/com/example/tests/ui/swiper/SwiperFragment.java
@@ -1,6 +1,7 @@
 package com.example.tests.ui.swiper;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,18 @@ import com.yuyakaido.android.cardstackview.Direction;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class SwiperFragment extends Fragment {
 
@@ -65,10 +78,48 @@ public class SwiperFragment extends Fragment {
 
     private List<Ingredient> createIngredients() {
         List<Ingredient> Ingredients = new ArrayList<>();
-        // Ajoutez vos profils ici, exemple :
+
+
         Ingredients.add(new Ingredient("Nom jjd d d f df r e er r r rz zr ezr er zer es sr s es ersserersersser se ers ser ers sre ers ser serersers rs ers srers1", R.drawable.ic_launcher_background));
         Ingredients.add(new Ingredient("Nom 2", R.drawable.ic_launcher_background));
         // et ainsi de suite...
+
+        RequestQueue queue = Volley.newRequestQueue(this.getContext());
+
+        String url = "http://10.0.2.2:80/ingredients/not_evaluated";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Traitement de la réponse
+                        // Exemple : textView.setText("Réponse : " + response.toString());
+                        try {
+                            JSONArray ingredients = response.getJSONArray("ingredients");
+                            for (int i = 0; i < ingredients.length(); i++) {
+                                String ingredient = ingredients.getString(i);
+
+                                // Affichage de l'ingrédient
+                                //System.out.println("Ingrédient: " + ingredient);
+                                Ingredients.add(new Ingredient(ingredient, R.drawable.ic_launcher_background));
+                            }
+                            adapter.notifyDataSetChanged(); // Informer l'adapter
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Gérer l'erreur
+                        Log.e("Volley", "Erreur: " + error.toString());
+                    }
+                });
+
+        // Ajouter la requête à la RequestQueue.
+        queue.add(jsonObjectRequest);
+
         return Ingredients;
     }
 
